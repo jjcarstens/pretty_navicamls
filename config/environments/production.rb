a59b0143f636e81e5e19b6f97ab9a01b914f765a
Jon Carstens <jon.carstens@mx.com>
2016-10-01 16:01:22 -0600

    Serve assets from public for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  # Serve files from the `/public` folder by default
+  config.public_file_server.enabled = true
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
